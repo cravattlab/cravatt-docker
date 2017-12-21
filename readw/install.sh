059cddd6a2209e0fd8b78d57eae8a53cc26adcac
Radu Suciu <radusuciu@gmail.com>
2017-12-20 18:26:15 -0800

    Functional conversion

--- readw/install.sh
+++ readw/install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+rm -rf ~/.win32
+
+# note that wineboot fails if DISPLAY is set to anything before it is run!
+DISPLAY= wineboot --init
 
 Xvfb :0 -screen 0 1024x768x16 &
-wineboot
 winetricks -q vcrun2008 vcrun2010
 regsvr32 readw/XRawfile2.dll
+
+DISPLAY= wineboot -e
+DISPLAY= wineboot -s
